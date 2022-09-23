bf437406263f15b067ecf14fdd504acd83702bc8
Muratop1gg <andreymur2008200430@gmail.com>
2022-09-23 12:37:42 +0300

    Generated Cmake project

--- 03_cpp/intro/out/build/x64-Debug/cmake_install.cmake
+++ 03_cpp/intro/out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/andre/-2022-polytech-muraviev/02_compiler
+# Install script for directory: C:/Users/andre/-2022-polytech-muraviev/03_cpp/intro
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ProjectName")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/andre/-2022-polytech-muraviev/03_cpp/intro/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/andre/-2022-polytech-muraviev/02_compiler/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/andre/-2022-polytech-muraviev/03_cpp/intro/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ 03_cpp/intro/program1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a;
+    cout << "Please enter your name" << endl;
+    cin >> a;
+    cout << "Hello, " << a << "!";
+    return 0; 
+}
