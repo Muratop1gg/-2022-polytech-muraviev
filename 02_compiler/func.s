4c310ac5c5249079e51a8b65b4e5683d2c298949
Muratop1gg <andreymur2008200430@gmail.com>
2022-11-18 10:35:19 +0300

    add(task): generated .s files

--- 02_compiler/func.s
+++ 02_compiler/func.s
@@ -1,10 +1,14 @@
-	.file	"hello_world.cpp"
+	.file	"func.cpp"
 	.text
 	.local	_ZStL8__ioinit
 	.comm	_ZStL8__ioinit,1,1
-	.globl	main
-	.type	main, @function
-main:
+	.section	.rodata
+.LC0:
+	.string	"Hello World!"
+	.text
+	.globl	_Z5printv
+	.type	_Z5printv, @function
+_Z5printv:
 .LFB1761:
 	.cfi_startproc
 	pushq	%rbp
@@ -12,17 +16,25 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	call	_Z5printv@PLT
-	movl	$0, %eax
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+	nop
 	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE1761:
-	.size	main, .-main
+	.size	_Z5printv, .-_Z5printv
 	.type	_Z41__static_initialization_and_destruction_0ii, @function
 _Z41__static_initialization_and_destruction_0ii:
-.LFB2285:
+.LFB2293:
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
@@ -33,9 +45,9 @@ _Z41__static_initialization_and_destruction_0ii:
 	movl	%edi, -4(%rbp)
 	movl	%esi, -8(%rbp)
 	cmpl	$1, -4(%rbp)
-	jne	.L5
+	jne	.L4
 	cmpl	$65535, -8(%rbp)
-	jne	.L5
+	jne	.L4
 	leaq	_ZStL8__ioinit(%rip), %rax
 	movq	%rax, %rdi
 	call	_ZNSt8ios_base4InitC1Ev@PLT
@@ -46,17 +58,17 @@ _Z41__static_initialization_and_destruction_0ii:
 	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
 	movq	%rax, %rdi
 	call	__cxa_atexit@PLT
-.L5:
+.L4:
 	nop
 	leave
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE2285:
+.LFE2293:
 	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
-	.type	_GLOBAL__sub_I_main, @function
-_GLOBAL__sub_I_main:
-.LFB2286:
+	.type	_GLOBAL__sub_I__Z5printv, @function
+_GLOBAL__sub_I__Z5printv:
+.LFB2294:
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
@@ -70,11 +82,11 @@ _GLOBAL__sub_I_main:
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE2286:
-	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
+.LFE2294:
+	.size	_GLOBAL__sub_I__Z5printv, .-_GLOBAL__sub_I__Z5printv
 	.section	.init_array,"aw"
 	.align 8
-	.quad	_GLOBAL__sub_I_main
+	.quad	_GLOBAL__sub_I__Z5printv
 	.hidden	__dso_handle
 	.ident	"GCC: (Debian 12.2.0-1) 12.2.0"
 	.section	.note.GNU-stack,"",@progbits
